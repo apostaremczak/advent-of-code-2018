32e12bdc1fd9b0aeb82d3c8e4dce9898ac342e32
Angelika Postaremczak <angelikapostar@gmail.com>
2018-12-01 15:17:14 +0100

    Use Set instead of Seq

--- src/main/scala/com/postaremczak/advent_of_code/day_01/ChronalCalibration.scala
+++ src/main/scala/com/postaremczak/advent_of_code/day_01/ChronalCalibration.scala
@@ -11,14 +11,15 @@ object ChronalCalibration {
     frequencyInput.sum
   }
 
-  def findRepeatedFrequency(frequenciesSeen: Seq[Int], frequencyInput: Seq[Int]): Int = {
-    val nextFrequency = frequenciesSeen.last + frequencyInput.head
+  def findRepeatedFrequency(frequencyInput: Seq[Int], frequenciesSeen: Set[Int] = Set(0), currentFrequency: Int = 0): Int = {
+    val nextFrequency = currentFrequency + frequencyInput.head
     if (frequenciesSeen.contains(nextFrequency)) {
       nextFrequency
     } else {
       findRepeatedFrequency(
-        frequenciesSeen.union(Seq(nextFrequency)),
-        frequencyInput.drop(1).union(Seq(frequencyInput.head))
+        frequencyInput.drop(1).union(Seq(frequencyInput.head)),
+        frequenciesSeen.union(Set(nextFrequency)),
+        nextFrequency
       )
     }
   }
@@ -30,7 +31,7 @@ object ChronalCalibration {
     // Part one
     println(s"Last frequency: ${findFinalFrequency(frequencyInput)}")
     // Part two
-    println(s"First repeated frequency: ${findRepeatedFrequency(Seq(0), frequencyInput)}")
+    println(s"First repeated frequency: ${findRepeatedFrequency(frequencyInput)}")
 
     bufferedFile.close()
   }
