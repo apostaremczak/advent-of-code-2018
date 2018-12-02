df28c6ddcf5444d90b6b6498c9b01bd8646b923c
Angelika Postaremczak <angelikapostar@gmail.com>
2018-12-02 16:48:26 +0100

    Make it a case class

--- src/main/scala/com/postaremczak/advent_of_code/PuzzleInput.scala
+++ src/main/scala/com/postaremczak/advent_of_code/PuzzleInput.scala
@@ -4,18 +4,9 @@ import java.io.InputStream
 
 import scala.io.Source
 
-class PuzzleInput(adventDay: Int) {
+case class PuzzleInput(adventDay: Int) {
   val fileName = s"day_${if (adventDay / 10 > 1) adventDay else "0" + adventDay}_input.txt"
   val stream: InputStream = getClass.getResourceAsStream(s"/$fileName")
-}
 
-object PuzzleInput {
-  def read(adventDay: Int): Seq[String] = {
-    val puzzle = new PuzzleInput(adventDay)
-    val bufferedFile = Source.fromInputStream(puzzle.stream)
-    val fileContent = bufferedFile.getLines().toSeq
-    bufferedFile.close()
-
-    fileContent
-  }
+  def read: Seq[String] = Source.fromInputStream(stream).getLines().toSeq
 }
