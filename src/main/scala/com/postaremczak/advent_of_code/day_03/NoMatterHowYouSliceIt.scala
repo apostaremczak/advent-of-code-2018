40d4a3772bc40510d5ca1052352f565f95879e98
Angelika Postaremczak <angelika@reality.co>
2018-12-03 12:46:12 +0100

    Finish part 2

--- src/main/scala/com/postaremczak/advent_of_code/day_03/NoMatterHowYouSliceIt.scala
+++ src/main/scala/com/postaremczak/advent_of_code/day_03/NoMatterHowYouSliceIt.scala
@@ -5,19 +5,37 @@ import com.postaremczak.advent_of_code.PuzzleInput
 
 object NoMatterHowYouSliceIt {
   val puzzleInput: PuzzleInput = PuzzleInput(3)
-  val fabricClaims: Seq[Option[FabricClaim]] = puzzleInput.read.map(FabricClaim(_))
+  val fabricClaims: Seq[FabricClaim] = puzzleInput.read.flatMap(FabricClaim(_))
 
-  def countOverlappingSquares: Int = {
+  def getOverlappingSquares: Seq[(Int, Int)] = {
     fabricClaims
-      .flatten
       .flatMap(_.getSquares)
       .groupBy(identity)
       .mapValues(_.size)
-      .count(_._2 > 1)
+      .filter(_._2 > 1)
+      .keys
+      .toSeq
+  }
+
+  def countOverlappingSquares(implicit overlappingSquares: Seq[(Int, Int)]): Int = {
+    overlappingSquares.size
+  }
+
+  def findNotOverlappingClaimId(implicit overlappingSquares: Seq[(Int, Int)]): Int = {
+    val overlapping = overlappingSquares.toSet
+    fabricClaims
+      .map { fabricClaim => (fabricClaim.id, fabricClaim.getSquares) }
+      .filter(_._2.toSet.intersect(overlapping).isEmpty)
+      .head
+      ._1
   }
 
   def main(args: Array[String]): Unit = {
+    implicit val overlappingSquares: Seq[(Int, Int)] = getOverlappingSquares
     // Part one
     println(s"Number of overlapping squares: $countOverlappingSquares")
+
+    // Part two
+    println(s"ID of the only not overlapping claim: $findNotOverlappingClaimId")
   }
 }
