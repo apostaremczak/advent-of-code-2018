9c8d5e95f7e2b179987479e41d63cce6dae417a1
Angelika Postaremczak <angelika@reality.co>
2018-12-03 12:00:11 +0100

    Read in fabric claims

--- src/main/scala/com/postaremczak/advent_of_code/day_03/NoMatterHowYouSliceIt.scala
+++ src/main/scala/com/postaremczak/advent_of_code/day_03/NoMatterHowYouSliceIt.scala
@@ -1,10 +1,17 @@
 package com.postaremczak.advent_of_code.day_03
 
 import com.postaremczak.advent_of_code.PuzzleInput
+import FabricClaim._
 
 
 object NoMatterHowYouSliceIt {
   val puzzleInput: PuzzleInput = PuzzleInput(3)
-  val fabricClaims: Seq[String] = puzzleInput.read
+  val fabricClaims: Seq[Either[TransformError, FabricClaim]] = puzzleInput.read.map(FabricClaim(_))
 
+  def countOverlappingSquares: Int = ???
+
+  def main(args: Array[String]): Unit = {
+    // Part one
+    println(s"Number of overlapping squares: $countOverlappingSquares")
+  }
 }
