38ab047eca09e34b608c1d6dc98b15c31bda0527
Angelika Postaremczak <angelika@reality.co>
2018-12-03 11:50:08 +0100

    Start day 3

---
+++ src/main/scala/com/postaremczak/advent_of_code/day_03/NoMatterHowYouSliceIt.scala
@@ -0,0 +1,10 @@
+package com.postaremczak.advent_of_code.day_03
+
+import com.postaremczak.advent_of_code.PuzzleInput
+
+
+object NoMatterHowYouSliceIt {
+  val puzzleInput: PuzzleInput = PuzzleInput(3)
+  val fabricClaims: Seq[String] = puzzleInput.read
+
+}
