974175994e886bd196e2c7e6bcb1f4945117c72e
Angelika Postaremczak <angelika@reality.co>
2018-12-06 17:36:48 +0100

    Refactor polymer scanning

--- src/main/scala/com/postaremczak/advent_of_code/day_05/AlchemicalReduction.scala
+++ src/main/scala/com/postaremczak/advent_of_code/day_05/AlchemicalReduction.scala
@@ -7,56 +7,28 @@ import scala.annotation.tailrec
 object AlchemicalReduction extends Solution(adventDay = 5) {
   val polymer: String = puzzleInput.read.head
 
-  def willUnitsReact(unit1: Char, unit2: Char): Boolean = {
-    if (unit1.toLower == unit2.toLower) {
-      val units = Seq(unit1, unit2)
-      units.exists(_.isLower) && units.exists(_.isUpper)
-    } else {
-      false
-    }
+  def willUnitsReact(u1: Char, u2: Char): Boolean = {
+    u1.toLower == u2.toLower && Seq(u1, u2).exists(_.isLower) && Seq(u1, u2).exists(_.isUpper)
   }
 
   @tailrec
-  def scanPolymer(toBeScanned: String, prev: Option[Char] = None, alreadyScanned: String = ""): String = {
+  def scanPolymer(toBeScanned: String, alreadyScanned: String = ""): String = {
     if (toBeScanned.isEmpty) {
       // Base case:
       // Nothing's left to be scanned, no more reactions will occur
-      alreadyScanned + prev.getOrElse("")
+      alreadyScanned
     } else {
-      // Look for any possible reactions
       val next = toBeScanned.head
-
-      if (prev.isDefined) {
-        // Remove those two characters and start from the beginning
-        if (willUnitsReact(prev.get, next)) {
-          if (alreadyScanned.isEmpty) {
-            scanPolymer(
-              toBeScanned.drop(1),
-              None,
-              alreadyScanned
-            )
-          } else {
-            scanPolymer(
-              toBeScanned.drop(1),
-              Some(alreadyScanned.last),
-              alreadyScanned.dropRight(1)
-            )
-          }
-        } else {
-          // Move to the next comparison and save previous unit
-          scanPolymer(
-            toBeScanned.drop(1),
-            Some(next),
-            alreadyScanned + prev.get
-          )
-        }
+      if (alreadyScanned.isEmpty) {
+        scanPolymer(toBeScanned.drop(1), next.toString)
       } else {
-        // Starting from the bottom now we're here
-        scanPolymer(
-          toBeScanned.drop(1),
-          Some(next),
-          alreadyScanned
-        )
+        // Look for any possible reactions
+        val prev = alreadyScanned.last
+        if (willUnitsReact(prev, next)) {
+          scanPolymer(toBeScanned.drop(1), alreadyScanned.dropRight(1))
+        } else {
+          scanPolymer(toBeScanned.drop(1), alreadyScanned + next)
+        }
       }
     }
   }
