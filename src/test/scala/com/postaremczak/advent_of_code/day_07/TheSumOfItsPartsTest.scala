c84b685d4d2dfa0107d98cacbef095f31b2d1bb6
Angelika Postaremczak <angelikapostar@gmail.com>
2018-12-08 21:15:23 +0100

    Add a test for the first puzzle part

--- src/test/scala/com/postaremczak/advent_of_code/day_07/TheSumOfItsPartsTest.scala
+++ src/test/scala/com/postaremczak/advent_of_code/day_07/TheSumOfItsPartsTest.scala
@@ -3,8 +3,8 @@ package com.postaremczak.advent_of_code.day_07
 import org.scalatest.FunSuite
 
 class TheSumOfItsPartsTest extends FunSuite {
-  test("correctFirst") {
-    assert(TheSumOfItsParts.findStepOrder == 'C')
+  test("TheSumOfItsParts.findStepOrder") {
+    assert(TheSumOfItsParts.findStepOrder == "CABDFE")
   }
 
 }
