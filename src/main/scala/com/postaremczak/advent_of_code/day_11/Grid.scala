4ef695fb5e04074dcddae2e9ce184b71638d946c
Angelika Postaremczak <angelika@reality.co>
2018-12-11 12:29:06 +0100

    Calculate total power of regions

--- src/main/scala/com/postaremczak/advent_of_code/day_11/Grid.scala
+++ src/main/scala/com/postaremczak/advent_of_code/day_11/Grid.scala
@@ -2,14 +2,35 @@ package com.postaremczak.advent_of_code.day_11
 
 
 case class Grid(serialNumber: Int) {
-  lazy val cells: Seq[Cell] = {
+
+  lazy val cells: Map[(Int, Int), Cell] = {
     Range(1, 301)
       .flatMap(
-        x => Range(1, 301)
-          .map(
-            y => Cell(x, y)(serialNumber)
-          )
+        x =>
+          Range(1, 301)
+            .map(
+              y => ((x, y), Cell(x, y)(serialNumber))
+            )
       )
+      .toMap
   }
 
+  def calculateRegionalPower(topLeftCoords: (Int, Int)): Int = {
+    // Region size: 3x3
+    val (minX, minY) = topLeftCoords
+
+    Range(minX, minX + 3)
+      .flatMap(
+        x =>
+          Range(minY, minY + 3)
+            .map(
+              y =>
+                cells.get((x, y)) match {
+                  case Some(cell) => cell.power
+                  case None => 0
+                }
+            )
+      )
+      .sum
+  }
 }
