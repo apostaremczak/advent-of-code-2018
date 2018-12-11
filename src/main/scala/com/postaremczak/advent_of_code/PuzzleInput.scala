5a387ef1d97fb85c7278224061486498bd1722b9
Angelika Postaremczak <angelika@reality.co>
2018-12-11 12:47:21 +0100

    Correct reading input files

--- src/main/scala/com/postaremczak/advent_of_code/PuzzleInput.scala
+++ src/main/scala/com/postaremczak/advent_of_code/PuzzleInput.scala
@@ -5,7 +5,7 @@ import java.io.InputStream
 import scala.io.Source
 
 case class PuzzleInput(adventDay: Int) {
-  val fileName = s"day_${if (adventDay / 10 > 1) adventDay else "0" + adventDay}_input.txt"
+  val fileName = s"day_${if (adventDay / 10 >= 1) adventDay else "0" + adventDay}_input.txt"
   val stream: InputStream = getClass.getResourceAsStream(s"/$fileName")
 
   def read: Seq[String] = Source.fromInputStream(stream).getLines().toSeq
