4ffc4363817dcbd1bd6597e9a27d393230d220b1
Angelika Postaremczak <angelika@reality.co>
2018-12-13 19:19:44 +0100

    Parse input

--- src/main/scala/com/postaremczak/advent_of_code/day_12/SubterraneanSustainability.scala
+++ src/main/scala/com/postaremczak/advent_of_code/day_12/SubterraneanSustainability.scala
@@ -3,5 +3,9 @@ package com.postaremczak.advent_of_code.day_12
 import com.postaremczak.advent_of_code.Solution
 
 object SubterraneanSustainability extends Solution(adventDay = 12) {
-  val rules = puzzleInput.read
+  val garden = Garden(puzzleInput.read)
+
+  def main(args: Array[String]): Unit = {
+    // Part one
+  }
 }
