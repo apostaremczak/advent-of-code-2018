e4e3f63b065e36bb6a382db7d46182fb2695bef2
Angelika Postaremczak <angelikapostar@gmail.com>
2018-12-14 11:15:35 +0100

    Add a method for making it a string

--- src/main/scala/com/postaremczak/advent_of_code/day_12/Garden.scala
+++ src/main/scala/com/postaremczak/advent_of_code/day_12/Garden.scala
@@ -13,21 +13,29 @@ case class Garden(
     Range(potIndex - 2, potIndex + 3).map(getPot)
   }
 
-  def evaluateGeneration(currentGarden: Garden): Garden = {
+  def evaluateGeneration: Garden = {
     // Check whether the garden's borders can be expanded
-    val toTheLeft = currentGarden.alignment.keys.min - 1
-    val toTheRight = currentGarden.alignment.keys.max + 1
+    val toTheLeft = alignment.keys.min - 1
+    val toTheRight = alignment.keys.max + 1
 
     Garden(
-      currentGarden
-        .alignment
-        .updated(toTheLeft, Pot.empty).updated(toTheRight, Pot.empty)
+      alignment
+        .updated(toTheLeft, Pot.empty)
+        .updated(toTheRight, Pot.empty)
         .keys
         .map {
           potIndex: Int => (potIndex, SurvivalRule.decide(getSurroundings(potIndex)))
         }
         .toMap
     )
+  }
+
+  def mkString: String = {
+    alignment
+      .toSeq
+      .sortWith((p1, p2) => p1._1 < p2._1)
+      .map { p => if (p._2.hasPlant) "#" else "." }
+      .mkString(sep = "")
   }
 }
 
